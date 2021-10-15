37ebcd3bf5a474382f518aa437ae6e5b5a7970bc
Stark Bots <88478059+StarkBotsIndustries@users.noreply.github.com>
2021-10-15 13:57:44 +0530

    v0.1.0
    1) Do not mess with formatting of posted messages.
    2) Handle FloodWaits.

--- ChannelBot/modify.py
+++ ChannelBot/modify.py
@@ -1,5 +1,7 @@
+import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup
+from pyrogram.errors.exceptions import FloodWait
 from ChannelBot.database.channel_sql import (
     get_caption,
     get_position,
@@ -26,38 +28,41 @@ async def modify(_, msg: Message):
     edit_mode = await get_edit_mode(channel_id)
     if edit_mode == 'media' and not msg.media:
         return
-    if caption:
-        position = await get_position(channel_id)
-        buttons = await get_buttons(channel_id)
-        if buttons:
-            buttons = await string_to_buttons(buttons)
-        webpage_preview = await get_webpage_preview(channel_id)
-        if position == 'above':
-            if msg.caption:
-                caption += '\n\n' + msg.caption
-            elif msg.text:
-                caption += '\n\n' + msg.text
-        elif position == 'below':
-            if msg.caption:
-                caption = msg.caption + '\n\n' + caption
-            elif msg.text:
-                caption = msg.text + '\n\n' + caption
-        if webpage_preview:
-            disable_webpage_preview = False
-        else:
-            disable_webpage_preview = True
-        if buttons:
-            await msg.edit_text(
-                caption,
-                reply_markup=InlineKeyboardMarkup(buttons),
-                disable_web_page_preview=disable_webpage_preview,
-                parse_mode="markdown"
-            )
-        else:
-            await msg.edit_text(
-                caption,
-                disable_web_page_preview=disable_webpage_preview,
-                parse_mode="markdown"
-            )
-    if sticker:
-        await msg.reply_sticker(sticker, quote=False)
+    try:
+        if caption:
+            position = await get_position(channel_id)
+            buttons = await get_buttons(channel_id)
+            if buttons:
+                buttons = await string_to_buttons(buttons)
+            webpage_preview = await get_webpage_preview(channel_id)
+            if position == 'above':
+                if msg.caption:
+                    caption += '\n\n' + msg.caption.markdown
+                elif msg.text:
+                    caption += '\n\n' + msg.text.markdown
+            elif position == 'below':
+                if msg.caption:
+                    caption = msg.caption.markdown + '\n\n' + caption
+                elif msg.text:
+                    caption = msg.text.markdown + '\n\n' + caption
+            if webpage_preview:
+                disable_webpage_preview = False
+            else:
+                disable_webpage_preview = True
+            if buttons:
+                await msg.edit_text(
+                    caption,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                    disable_web_page_preview=disable_webpage_preview,
+                    parse_mode="markdown"
+                )
+            else:
+                await msg.edit_text(
+                    caption,
+                    disable_web_page_preview=disable_webpage_preview,
+                    parse_mode="markdown"
+                )
+        if sticker:
+            await msg.reply_sticker(sticker, quote=False)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
